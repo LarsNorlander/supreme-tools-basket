63ea2a363b5d9bf71cb634f8060a21701cca35d9
Claes Eklund <claekl.eklund@gmail.com>
2017-05-27 10:37:31 +0200

    Added cmake build essetianls for youcompleteme

--- setupVim.sh
+++ setupVim.sh
@@ -7,9 +7,10 @@ if [ $status -ne 0 ]; then
   sudo apt install vim
 fi 
 
-sudo apt install cmake
-sudo apt install g++
-
+sudo apt -y install cmake
+sudo apt -y install g++
+sudo apt-get install build-essential cmake
+sudo apt-get install python-dev python3-dev
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sf ${DIR}/.vim ~/.vim
@@ -18,6 +19,8 @@ ln -sf ${DIR}/.vimrc ~/.vimrc
 curl -fLo ${DIR}/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim -c "PlugInstall|update|quit|quit"
+
+./${DIR}/pluuged/YouCompleteMe/install.py
 
 #vim -E -s dummy.out << EOF
 #:PlugInstall
